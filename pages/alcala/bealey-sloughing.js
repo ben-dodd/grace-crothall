d1f3c5017bcb1c4b5338927552836cca367e1516
Ben D <thebillham@gmail.com>
2021-05-07 22:05:18 +1200

    Video controls

--- pages/alcala/bealey-sloughing.js
+++ pages/alcala/bealey-sloughing.js
@@ -4,42 +4,75 @@ import Link from "next/link";
 import Layout from "../../components/layout";
 import ReactPlayer from "react-player";
 import { useMediaQuery } from "../../lib/mediaQuery";
+import { PlayArrow, Pause, VolumeOff, VolumeUp } from "react-icons/md";
 
 export default function BealeySloughing() {
   const isLargeScreen = useMediaQuery(`(min-width:1024px)`);
   const isMedScreen = useMediaQuery(`(min-width:768px)`);
+  const [isPlaying, setIsPlaying] = useState(true);
+  const [isMuted, setIsMuted] = useState(false);
 
   return (
     <Layout>
       <div className="flex-grow flex w-screen">
         {isLargeScreen ? (
           <div className="mx-auto place-self-center bg-black p-16">
+            <div className="flex">
+              <button onClick={() => setIsPlaying((current) => !current)}>
+                {isPlaying ? <Pause /> : <PlayArrow />}
+              </button>
+              <button onClick={() => setIsMuted((current) => !current)}>
+                {isMuted ? <VolumeOff /> : <VolumeUp />}
+              </button>
+            </div>
             <ReactPlayer
               url="https://storage.googleapis.com/grace-crothall/video/bealey-sloughing-480p.mp4"
-              playing={true}
+              playing={isPlaying}
               loop={true}
               width="854px"
               height="480px"
+              volume={1}
+              muted={isMuted}
             />
           </div>
         ) : isMedScreen ? (
           <div className="mx-auto place-self-center bg-black p-12">
+            <div className="flex">
+              <button onClick={() => setIsPlaying((current) => !current)}>
+                {isPlaying ? <Pause /> : <PlayArrow />}
+              </button>
+              <button onClick={() => setIsMuted((current) => !current)}>
+                {isMuted ? <VolumeOff /> : <VolumeUp />}
+              </button>
+            </div>
             <ReactPlayer
               url="https://storage.googleapis.com/grace-crothall/video/bealey-sloughing-360p.mp4"
-              playing={true}
+              playing={isPlaying}
               loop={true}
               width="640px"
               height="360px"
+              volume={1}
+              muted={isMuted}
             />
           </div>
         ) : (
           <div className="mx-auto place-self-center bg-black p-8">
+            <div className="flex">
+              <button onClick={() => setIsPlaying((current) => !current)}>
+                {isPlaying ? <Pause /> : <PlayArrow />}
+              </button>
+              <button onClick={() => setIsMuted((current) => !current)}>
+                {isMuted ? <VolumeOff /> : <VolumeUp />}
+              </button>
+            </div>
             <ReactPlayer
               url="https://storage.googleapis.com/grace-crothall/video/bealey-sloughing-240p.mp4"
-              playing={true}
+              playing={isPlaying}
               loop={true}
               width="426px"
               height="240px"
+              volume={1}
+              muted={isMuted}
             />
           </div>
         )}
