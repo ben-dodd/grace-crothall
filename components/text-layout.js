41fc911155aa83e146ee8ca661b6f0e9e353062f
Ben D <thebillham@gmail.com>
2021-07-26 23:49:15 +1200

    scroll

--- components/text-layout.js
+++ components/text-layout.js
@@ -1,6 +1,6 @@
 export default function TextLayout({ title, author, pdfLink, children }) {
   return (
-    <div className="flex-grow overflow-y-scroll">
+    <div className="flex-grow">
       <div className="text-3xl text-center font-bold mt-8 mb-4">{title}</div>
       <div className="text-2xl text-center mb-16">{author}</div>
       <div className="mx-auto max-w-2xl px-4">{children}</div>
