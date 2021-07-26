fadf00bc2dde616a56a3839933dbfe4a965821ab
Ben D <thebillham@gmail.com>
2021-07-27 00:34:14 +1200

    fix 500 error

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -4,24 +4,24 @@ import fs from "fs";
 const Sitemap = () => {};
 
 export const getServerSideProps = ({ res }) => {
-  const baseUrl = {
-    development: "http://localhost:5000",
-    production: "https://mydomain.com",
-  }[process.env.NODE_ENV];
+  // const baseUrl = {
+  //   development: "http://localhost:5000",
+  //   production: "https://mydomain.com",
+  // }[process.env.NODE_ENV];
 
-  const staticPages = fs
-    .readdirSync("pages")
-    .filter((staticPage) => {
-      return ![
-        "_app.js",
-        "_document.js",
-        "_error.js",
-        "sitemap.xml.js",
-      ].includes(staticPage);
-    })
-    .map((staticPagePath) => {
-      return `${baseUrl}/${staticPagePath}`;
-    });
+  // const staticPages = fs
+  //   .readdirSync("pages")
+  //   .filter((staticPage) => {
+  //     return ![
+  //       "_app.js",
+  //       "_document.js",
+  //       "_error.js",
+  //       "sitemap.xml.js",
+  //     ].includes(staticPage);
+  //   })
+  //   .map((staticPagePath) => {
+  //     return `${baseUrl}/${staticPagePath}`;
+  //   });
 
   // const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   //   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
