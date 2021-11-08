2c467dd3b553e72341a6f1171664d3e413253d72
Ben D <thebillham@gmail.com>
2021-11-08 22:38:19 +1300

    site map update

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -4,70 +4,34 @@ import fs from "fs";
 const Sitemap = () => {};
 
 export const getServerSideProps = ({ res }) => {
-  // const baseUrl = {
-  //   development: "http://localhost:5000",
-  //   production: "https://mydomain.com",
-  // }[process.env.NODE_ENV];
-
-  // const staticPages = fs
-  //   .readdirSync("pages")
-  //   .filter((staticPage) => {
-  //     return ![
-  //       "_app.js",
-  //       "_document.js",
-  //       "_error.js",
-  //       "sitemap.xml.js",
-  //     ].includes(staticPage);
-  //   })
-  //   .map((staticPagePath) => {
-  //     return `${baseUrl}/${staticPagePath}`;
-  //   });
-
-  // const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  //   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  //     ${staticPages
-  //       .map((url) => {
-  //         return `
-  //           <url>
-  //             <loc>${url}</loc>
-  //             <lastmod>${new Date().toISOString()}</lastmod>
-  //             <changefreq>monthly</changefreq>
-  //             <priority>1.0</priority>
-  //           </url>
-  //         `;
-  //       })
-  //       .join("")}
-  //   </urlset>
-  // `;
-
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
       <loc>https://www.gracecrothall.com/index.js</loc>
-      <lastmod>2021-07-26T12:09:30.532Z</lastmod>
+      <lastmod>2021-11-08T12:09:30.532Z</lastmod>
       <changefreq>monthly</changefreq>
       <priority>1.0</priority>
     </url>
     <url>
       <loc>https://www.gracecrothall.com/alcala.js</loc>
-      <lastmod>2021-07-26T12:09:30.532Z</lastmod>
+      <lastmod>2021-11-08T12:09:30.532Z</lastmod>
       <changefreq>monthly</changefreq>
       <priority>0.8</priority>
     </url>
     <url>
       <loc>https://www.gracecrothall.com/shelterhouse.js</loc>
-      <lastmod>2021-07-26T12:09:30.532Z</lastmod>
+      <lastmod>2021-11-08T12:09:30.532Z</lastmod>
       <changefreq>monthly</changefreq>
       <priority>0.8</priority>
     </url>
     <url>
       <loc>https://www.gracecrothall.com/text/index.js</loc>
-      <lastmod>2021-07-26T12:09:30.532Z</lastmod>
+      <lastmod>2021-11-08T12:09:30.532Z</lastmod>
       <changefreq>monthly</changefreq>
       <priority>0.8</priority>
     </url>
     <url>
       <loc>https://www.gracecrothall.com/text/building-as-body.js</loc>
-      <lastmod>2021-07-26T12:09:30.532Z</lastmod>
+      <lastmod>2021-11-08T12:09:30.532Z</lastmod>
       <changefreq>monthly</changefreq>
       <priority>0.5</priority>
     </url>
@@ -75,25 +39,19 @@ export const getServerSideProps = ({ res }) => {
       <loc>
         https://www.gracecrothall.com/text/the-milk-carton-kids-of-the-80s.js
       </loc>
-      <lastmod>2021-07-26T12:09:30.532Z</lastmod>
+      <lastmod>2021-11-08T12:09:30.532Z</lastmod>
       <changefreq>monthly</changefreq>
       <priority>0.5</priority>
     </url>
     <url>
       <loc>https://www.gracecrothall.com/alcala/bealey-sloughing.js</loc>
-      <lastmod>2021-07-26T12:09:30.532Z</lastmod>
+      <lastmod>2021-11-08T12:09:30.532Z</lastmod>
       <changefreq>monthly</changefreq>
       <priority>0.5</priority>
     </url>
     <url>
       <loc>https://www.gracecrothall.com/alcala/install-view.js</loc>
-      <lastmod>2021-07-26T12:09:30.532Z</lastmod>
-      <changefreq>monthly</changefreq>
-      <priority>0.5</priority>
-    </url>
-    <url>
-      <loc>https://www.gracecrothall.com/alcala/snakes-of-new-zealand.js</loc>
-      <lastmod>2021-07-26T12:09:30.532Z</lastmod>
+      <lastmod>2021-11-08T12:09:30.532Z</lastmod>
       <changefreq>monthly</changefreq>
       <priority>0.5</priority>
     </url>
