c67bfe88205eda205457bb6fc59b63bda820138b
Ben D <thebillham@gmail.com>
2021-11-14 20:13:03 +1300

    Added email

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="border-t border-accent-5">
-      <div className="text-xs text-center md:text-right md:pr-2">
-        © Copyright Grace Crothall 2019.
+    <footer className="border-t border-accent-5 flex px-2 justify-between">
+      <div className="text-xs text-center underline">
+        <a href="mailto:gracecrothall@gmail.com">gracecrothall@gmail.com</a>
       </div>
+      <div className="text-xs self-end">© Copyright Grace Crothall 2019.</div>
     </footer>
   );
 }
