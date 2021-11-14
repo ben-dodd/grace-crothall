c02a2921b09ad200ea8634d7bf861fbe56c835e8
Ben D <thebillham@gmail.com>
2021-11-14 20:08:43 +1300

    Text

--- components/text-layout.js
+++ components/text-layout.js
@@ -8,10 +8,14 @@ export default function TextLayout({
   children,
 }) {
   return (
-    <div className="flex-grow">
-      <h1 className="text-3xl font-bold mt-8 mb-4">{title}</h1>
-      {subtitle && <h2 className="text-xl mb-4">{subtitle}</h2>}
-      <div className="text-2xl mb-16">{author}</div>
+    <div className="flex-grow px-2">
+      <h1 className="text-3xl max-w-4xl mx-auto font-bold mt-8 mb-2">
+        {title}
+      </h1>
+      {subtitle && (
+        <h2 className="text-xl max-w-4xl mx-auto mb-4">{subtitle}</h2>
+      )}
+      <div className="text-2xl max-w-4xl mx-auto mb-16">{author}</div>
       <div className="mx-auto max-w-2xl px-4">
         {children}
         {pdfLink && (
